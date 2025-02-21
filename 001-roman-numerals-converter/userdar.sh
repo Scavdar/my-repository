d7d6ceeceda8a2ff70520fdce7f5f68f98d8fd63
Scavdar <111649887+Scavdar@users.noreply.github.com>
2025-02-20 20:57:22 -0500

    Update userdar.sh

--- 001-roman-numerals-converter/userdar.sh
+++ 001-roman-numerals-converter/userdar.sh
@@ -8,30 +8,6 @@ yum install python3 -y
 yum install pip -y
 pip install flask
 
-# insatall the codeo
-
-#change to user directory
-cd /home/ec2-user
-
-#get the app.py file into current dir
-FOLDER= "https://raw.githubusercontent.com/Scavdar/my-repository/refs/heads/main/001-roman-numerals-converter"
-wget ${FOLDER}/app.py
-
-#get the template into the tempaltes/ directory
-mkdir templates
-cd templates
-wget ${FOLDER}/templates/index.html
-wget ${FOLDER}/templates/result.html
-#!/bin/bash -x
-
-# update system
-yum update -y
-
-# install Python and flask
-yum install python3 -y
-yum install pip -y
-pip install flask
-
 #### install the code  ###
 
 # change to the user directory
